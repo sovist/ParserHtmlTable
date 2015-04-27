02e351e09f7c3e4e0907b62e460e5db77501828a
sovist <sovist9@ukr.net>
2015-04-27 18:08:52 +0300

    ParserHtmlTable

--- ParserHtmlTable.cs
+++ ParserHtmlTable.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace HtmlDocument
 {
     class ParserHtmlTable
     {
